379f58b8c9a3e0ca98caacc5fd3b10dfddc899aa
Титов Валентин <vtitov@truenorth.pro>
2017-07-17 16:41:21 +0300

    Repaired 1 and 2 task

--- src/com/titov/Main.java
+++ src/com/titov/Main.java
@@ -10,17 +10,20 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-/** В комментарии вызов реализации первых двух задач из домашнего задания
- *
-        Object[] arr = new Object[]{1,2,4,6};
-        int size = arr.length;
-        Object[] newArray = replace(arr,1,3);
+        int size = 5;
+        SomeClass<Object>[] arr = new SomeClass[size];
         for (int i = 0; i < size; i++) {
-            System.out.print(newArray[i] + ", ");
+            arr[i] = new SomeClass<>(i);
         }
-        ArrayList list = toArrayList(newArray);
-        System.out.println("\n" + list.toString());
- */
+        System.out.println("\nSimple array:");
+        SomeClass<Object>[] newArray = replace(arr,1,3);
+        for (int i = 0; i < size; i++) {
+            System.out.print(newArray[i].getObj() + ", ");
+        }
+        System.out.println("\nArray list:");
+        ArrayList<SomeClass<Object>> list = toArrayList(newArray);
+        System.out.println(list.toString());
+/** В комментарии вызов 3-ей части домашнего задания
         Box<Orange> box1 = generatedOrangesBox(4);
         Box<Orange> box2 = generatedOrangesBox(7);
         Box<Apple> box3 = generatedAppleBox(6);
@@ -28,10 +31,9 @@ public class Main {
         System.out.println("Is weight of box1 equal to box3? - " + box1.compareTo(box3));
         System.out.println("Pour box1 with weight " + box1.getWeight() + " to box2 with weight " + box2.getWeight());
         System.out.println("New weight of box2: " + box1.pourTo(box2).getWeight());
-        System.out.println("Weight of box1: " + box1.getWeight());
+        System.out.println("Weight of box1: " + box1.getWeight());*/
 // Попытка пересыпать апельсины в яблоки. При раскомментировании должна выдаваться ошибка
 //        box1.pourTo(box3);
-
     }
 
     public static Box<Orange> generatedOrangesBox(int size) {
@@ -51,8 +53,21 @@ public class Main {
     }
 
 
+
 /** Part 1 */
-    private static Object[] replace(Object[] array, int srcPosition, int dstPosition) {
+    private static class SomeClass<T extends Object> {
+        T obj;
+        public SomeClass(T obj) {
+            this.obj = obj;
+        }
+
+        public T getObj() { return obj; }
+
+        @Override
+        public String toString() { return obj.toString(); }
+    }
+
+    private static SomeClass<Object>[] replace(SomeClass<Object>[] array, int srcPosition, int dstPosition) {
         int size = array.length;
         if (srcPosition < 0 || srcPosition >=size) {
             throw new RuntimeException("Bad source element position: " + srcPosition);
@@ -60,18 +75,18 @@ public class Main {
         if (dstPosition < 0  || dstPosition >= size) {
             throw new RuntimeException("Bad destination element position: " + dstPosition);
         }
-        Object tmp = array[srcPosition];
+        SomeClass<Object> tmp = array[srcPosition];
         array[srcPosition] = array[dstPosition];
         array[dstPosition] = tmp;
         return array;
     }
 
-    private static ArrayList<Object> toArrayList(Object[] array) {
+    private static ArrayList<SomeClass<Object>> toArrayList(SomeClass<Object>[] array) {
         if (array == null) {
             throw new RuntimeException("null array as argument");
         }
         int size = array.length;
-        ArrayList<Object> list = new ArrayList<>(size);
+        ArrayList<SomeClass<Object>> list = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
             list.add(array[i]);
         }
