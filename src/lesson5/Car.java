24b9d0d82aadd78daf8219e6fa76d52c88ab9f84
Титов Валентин <vtitov@truenorth.pro>
2017-07-31 09:41:59 +0300

    ДЗ-5. Реализованы все задания

--- src/lesson5/Car.java
+++ src/lesson5/Car.java
@@ -11,16 +11,18 @@ public class Car implements Runnable {
     private String name;
     private CyclicBarrier barrier;
     private CountDownLatch ready;
+    private CountDownLatch finished;
     private static int CARS_COUNT = 0;
     private static final int REQUIRE_WAITING_TIME = 500;
     private static final int MAX_WAITING_TIME = 800;
 
 /** Constructors */
-    public Car(Race race, int speed, CyclicBarrier barrier, CountDownLatch ready) {
+    public Car(Race race, int speed, CyclicBarrier barrier, CountDownLatch ready,CountDownLatch finished) {
         this.race = race;
         this.speed = speed;
         this.barrier = barrier;
         this.ready = ready;
+        this.finished = finished;
         CARS_COUNT ++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -34,8 +36,8 @@ public class Car implements Runnable {
         try {
             Thread.sleep(REQUIRE_WAITING_TIME + (int)Math.random() * MAX_WAITING_TIME);
             System.out.println(name + " готов");
-            barrier.await();
             ready.countDown();
+            barrier.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (BrokenBarrierException e) {
@@ -46,6 +48,8 @@ public class Car implements Runnable {
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        System.out.println(name + " завершил гонку");
+        finished.countDown();
     }
 
 /** Getters */
--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -5,12 +5,12 @@ import java.util.concurrent.CyclicBarrier;
 
 public class Main {
 
-    public static final int CARS_COUNT = 8;
+    public static final int CARS_COUNT = 4;
     private static final int MIN_CAR_SPEED = 20;
     private static final int DELTA_CAR_SPEED = 10;
     /** Race parameters */
     public static final int FIRST_ROAD_LENGTH = 100;
-    public static final int SECOND_ROAD_LENGTH = 500;
+    public static final int SECOND_ROAD_LENGTH = 300;
     public static final int THIRD_ROAD_LENGTH = 100;
     public static final int FIRST_TUNNEL_THROUGHPUT = 2;
     public static final int SECOND_TUNNEL_THROUGHPUT = 3;
@@ -24,14 +24,21 @@ public class Main {
         Car[] cars = new Car[CARS_COUNT];
         CyclicBarrier barrier = new CyclicBarrier(CARS_COUNT);
         CountDownLatch readyLatch = new CountDownLatch(CARS_COUNT);
+        CountDownLatch finished = new CountDownLatch(CARS_COUNT);
         for (int i = 0; i < CARS_COUNT; i++) {
-            cars[i] = new Car(race,MIN_CAR_SPEED + (int) (Math.random() * DELTA_CAR_SPEED),barrier,readyLatch);
+            cars[i] = new Car(race,MIN_CAR_SPEED + (int) (Math.random() * DELTA_CAR_SPEED),barrier,readyLatch,finished);
         }
         for (int i = 0; i < CARS_COUNT; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Гонка закончилась!!!");
+        try {
+            readyLatch.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Гонка началась!!!");
+            finished.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЕНИЕ >>> Гонка закончилась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
